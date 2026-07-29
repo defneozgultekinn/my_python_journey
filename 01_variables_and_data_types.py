ffb832f782d38068e430362e6aaff66d2dedde77
Defne Özgültekin <defneozgultekin@192.168.1.34>
2026-07-29 21:04:42 +0200

    day 1: variables and data types

---
+++ 01_variables_and_data_types.py
@@ -0,0 +1,19 @@
+name= "defne"   #str
+age=21   #int
+is_student= True  #boolean
+height= 1.66   #float
+#let's print these
+print(name)
+print(age)
+print(is_student)
+print(height)
+
+print("My mom turned 60 this year,")
+print("so she celebrated 60 birthdays in her life")
+
+mom_age1= "50"  #string
+print("My mom turned " + mom_age1 + " this year,")
+print("so she celebrated " + mom_age1 + " birthdays in her life")
+
+mom_age2 = 70
+print(f"My mom turned {mom_age2} this year")  #f string
