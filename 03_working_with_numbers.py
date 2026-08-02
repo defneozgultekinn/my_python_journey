727ee55e283baed80e64cbccb7a7b9caa9af9155
Defne Özgültekin <defneozgultekin@192.168.1.48>
2026-08-02 14:29:09 +0200

    day 3: working with numbers

---
+++ 03_working_with_numbers.py
@@ -0,0 +1,25 @@
+print(10%3)  #10 mod 3
+
+my_num= 5
+print(str(my_num))  #converting a number to a string
+print(str(my_num) + " is my fav number ")  #we cant use numbers and strings in the same time so we have to convert
+
+#getting the absolute value of a num
+my_num2= -5
+print(abs(my_num2))
+
+#finding the power of a number
+print(pow(2,4))
+print(pow(2567,4))
+
+#finding the bigger num
+print(max(3,9))
+#same for smaller
+print(min(3,9))
+
+#rounding a number
+print(round(6.89))   #7
+
+from math import *    #this allows us to use more math functions
+print(floor(3.7))  #3
+print(ceil(3.7))   #4
