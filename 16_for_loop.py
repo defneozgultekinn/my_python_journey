d0df4469b6b189568e38a8a4401e3cabe1a087eb
Defne Özgültekin <defneozgultekin@192.168.1.44>
2026-08-30 22:00:41 +0200

    day 16: for loop

---
+++ 16_for_loop.py
@@ -0,0 +1,14 @@
+for letter in "defne":
+    print(letter)         #this prints out every letter in the string
+
+for index in range(20):
+    print(index)    #from 0 to 20. not including 20
+
+
+for index in range(4,18):  #from 4 to 18, NOT includiing 18
+    print(index)
+
+
+names=["Defne", "Jake", "Chris"]
+for name in range(len(names)):
+    print(names[name])
