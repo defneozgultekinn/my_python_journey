fd7e10f4ac70ca9e58e82aa38fb0842eebaa9514
Defne Özgültekin <defneozgultekin@192.168.1.34>
2026-08-31 12:29:36 +0200

    day 17: exponent function

---
+++ 17_exponent_function.py
@@ -0,0 +1,13 @@
+
+def power(base_num,raise_num):
+    result=1
+    for x in range(raise_num):
+       result=result* base_num
+    return result       #RETURN SHOULD BE OUT OF THE LOOP!!!
+
+print(power(2,3))
+
+
+
+
+
